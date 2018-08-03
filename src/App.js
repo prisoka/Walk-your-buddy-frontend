de6ba84c13a628666dd7157b78a3a3c36e988afb
Priscilla Okawa <prisoka@gmail.com>
2018-08-02 17:53:03 -0700

    setup css of login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/headerComponent'
 import Footer from './components/footerComponent'
+import Login from './components/loginComponent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Login />
         <Footer />
       </div>
     );
--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -3,41 +3,53 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-      <section class="hero is-success is-fullheight">
+      <section class="hero is-fullheight">
         <div class="hero-body">
         <div class="container has-text-centered">
           <div class="column is-4 is-offset-4">
             <h3 class="title has-text-grey">Login</h3>
-            <p class="subtitle has-text-grey">Please login to proceed.</p>
+            <p class="subtitle has-text-grey">Login to access your WYB account.</p>
             <div class="box">
               <figure class="avatar">
-              <img src="https://placehold.it/128x128" />
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" />
               </figure>
               <form>
-              <div class="field">
-                <div class="control">
-                  <input class="input is-large" type="email" placeholder="Your Email" autofocus="" />
+                <div class="field">
+                  <p class="control has-icons-left has-icons-right">
+                    <input id='login_email-field' class="input" type="email" placeholder="Email" required />
+                    <span class="icon is-small is-left">
+                      <i class="fas fa-envelope"></i>
+                    </span>
+                    <span class="icon is-small is-right">
+                      <i class="fas fa-check"></i>
+                    </span>
+                  </p>
                 </div>
-              </div>
 
-              <div class="field">
-                <div class="control">
-                  <input class="input is-large" type="password" placeholder="Your Password" />
+                <div class="field">
+                  <p class="control has-icons-left">
+                    <input id='login_password-field' class="input" type="password" placeholder="Password" required />
+                    <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                    </span>
+                  </p>
                 </div>
-              </div>
-              <div class="field">
-                <label class="checkbox">
-                  <input type="checkbox" />
-                  Remember me
-                </label>
-              </div>
-              <button class="button is-block is-info is-large is-fullwidth">Login</button>
+                <div class="field">
+                  <label class="checkbox">
+                    <input type="checkbox" />
+                    Remember me
+                  </label>
+                </div>
+                <div class="field is-grouped is-grouped-centered">
+                  <p class="control">
+                    <input id="login_submit-form" type='submit' class="button is-primary is-rounded" value="Login" />
+                  </p>
+                </div>
               </form>
             </div>
             <p class="has-text-grey">
-            <a href="../">Sign Up</a> &nbsp;·&nbsp;
-            <a href="../">Forgot Password</a> &nbsp;·&nbsp;
-            <a href="../">Need Help?</a>
+            <a href="../">Signup</a> &nbsp;·&nbsp;
+            <a href="../">Forgot password</a>
             </p>
           </div>
         </div>
