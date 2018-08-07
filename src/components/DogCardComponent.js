4222b0984460b43b342757146b7ac308a97730df
Priscilla Okawa <prisoka@gmail.com>
2018-08-07 11:44:16 -0700

    fixed react warnings

--- src/components/DogCardComponent.js
+++ src/components/DogCardComponent.js
@@ -4,37 +4,33 @@ class DogCard extends Component {
 
   render() {
     return (
-      <div class="column is-one-quarter">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+      <div className="column is-one-quarter">
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="dog" />
             </figure>
           </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="user" />
                 </figure>
               </div>
-              <div class="media-content">
-                <p class="title is-4">John Smith</p>
-                <p class="subtitle is-6">@johnsmith</p>
+              <div className="media-content">
+                <p className="title is-4">User: Priscilla</p>
+                <p className="subtitle is-6">Dog: Aquila</p>
               </div>
             </div>
 
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-              <a href="#">#css</a> <a href="#">#responsive</a>
-              <br />
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            <div className="content">
+              <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
             </div>
           </div>
-            <footer class="card-footer">
-            <a href="#" class="card-footer-item">Accept</a>
-            <a href="#" class="card-footer-item">Decline</a>
+            <footer className="card-footer">
+            <a href="#" className="card-footer-item">Accept</a>
+            <a href="#" className="card-footer-item">Decline</a>
           </footer>
         </div>
       </div>
