284855fd9977dc9489540b0c65427d2d81f5f9d1
Priscilla Okawa <prisoka@gmail.com>
2018-08-07 22:59:06 -0700

    frontend supports Cookie and Header Authorization

--- src/components/UserIndexComponent.js
+++ src/components/UserIndexComponent.js
@@ -12,34 +12,34 @@ class UserIndex extends Component {
                 Hi, USER_NAME!
               </h1>
               <br/>
-              <p class="has-text-centered">
+              <p className="has-text-centered">
                 <Link to="/" className="button is-warning is-inverted is-rounded is-focused is-medium">Book A Walk</Link> &nbsp;
               </p>
             </div>
           </div>
         </section>
         <section>
-          <div class="hero-body">
-            <div class="container has-text-centered">
-              <div class="columns is-centered">
-                <div class="column is-6 is-offset-1">
-                  <h2 class="subtitle is-4">
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <div className="columns is-centered">
+                <div className="column is-6 is-offset-1">
+                  <h2 className="subtitle is-4">
                     Available walkers near you:
                   </h2>
-                  <p class="has-text-centered">
+                  <p className="has-text-centered">
                     <Link to="/add_dog" className="button is-warning is-medium">Add Your Dog</Link>
                   </p>
                   <br/>
-                  <p class="has-text-centered">
+                  <p className="has-text-centered">
                     <Link to="/" className="button is-warning is-medium">Upcoming Walks</Link>
                   </p>
                   <br/>
-                  <p class="has-text-centered">
+                  <p className="has-text-centered">
                     <Link to="/" className="button is-warning is-medium">Past Walks</Link>
                   </p>
                 </div>
-                <div class="column is-5">
-                  <figure class="image is-4by3">
+                <div className="column is-5">
+                  <figure className="image is-4by3">
                     <img src="https://picsum.photos/800/600/?random" alt="Description" />
                   </figure>
                 </div>
---
+++ src/utils/getCookie.js
@@ -0,0 +1,6 @@
+export const getCookie = function getCookie(cookiename) {
+  // Get name followed by anything except a semicolon
+  var cookiestring=RegExp(""+cookiename+"[^;]+").exec(document.cookie);
+  // Return everything after the equal sign, or an empty string if the cookie name not found
+  return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
+}
